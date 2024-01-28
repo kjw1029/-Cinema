4a218b5a965c7fb078d4b5e03142638b6c5200b4
kang <kang@DESKTOP-2UCP4GJ>
2024-01-28 19:00:01 +0900

    encoding="UTF-8"

--- 240128/src/Store.java
+++ 240128/src/Store.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.event.MenuListener;
 
 public class Store {
 	private Food food;
 	private GifrCard giftCard;
-	private Map<Integer, List<Food>> menuList;
+	private static Map<Integer, List<Food>> menuList = new HashMap<>(); // 세트메뉴 선택
 	
-	public static void menuRegistration() {
-		
+	public static void main(String[] args) {
+		new Store();
 	}
 }
