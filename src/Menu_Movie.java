ce7f972f5f8d2c3622d43a8f0f4b5be9fa0c0fc4
sun <sunyoung.eom@outlook.com>
2024-02-06 17:44:33 +0900

    추가

--- src/Menu_Movie.java
+++ src/Menu_Movie.java
@@ -1,5 +1,31 @@
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Menu_Movie extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
+public class Menu_Movie extends JPanel {
+	public Menu_Movie() {
+		JButton switchButton = new JButton("메인");
+		switchButton.setBounds(12, 10, 131, 23);
+		add(switchButton);
+
+		switchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false); 
+			}
+		});
+//		System.out.println(MovieProgram.lblNewLabel_3.getText());
+		if (!MovieProgram.lblNewLabel_3.getText().equals("로그인 해주세요")) {
+			int length = MovieProgram.lblNewLabel_3.getText().length();
+		System.out.println(MovieProgram.lblNewLabel_3.getText().substring(0, length - 7));
+		} else {
+			
+			System.out.println(MovieProgram.lblNewLabel_3.getText());
+		}
+		System.out.println("length: " + MovieProgram.lblNewLabel_3.getText().length());
+		
+		
+	}
 }
